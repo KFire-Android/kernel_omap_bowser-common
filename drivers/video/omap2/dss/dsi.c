3a9af724739ba89bce3ef154649121c2210611dd
Taras Kondratiuk <taras@ti.com>
2012-08-08 00:21:27 -0500

    OMAPDSS: DSS: Scale DSS OPP according to pixel clock rate
    Before enabling pixel clock DSS should set its OPP
    for OPP framework to set correct VDD_CORE voltage level.
    
    When pixel clock is disabled DSS can set the lowest OPP.
    
    Change-Id: Ia1459614ae306dba9b048b149e474442e2880bba
    Signed-off-by: Taras Kondratiuk <taras@ti.com>
    
    Conflicts:
    	drivers/video/omap2/dss/dsi.c

--- drivers/video/omap2/dss/dsi.c
+++ drivers/video/omap2/dss/dsi.c
@@ -42,6 +42,7 @@
 #include <video/omapdss.h>
 #include <plat/clock.h>
 #include <plat/omap_apps_brd_id.h>
+#include <plat/omap_hwmod.h>
 
 #include "dss.h"
 #include "dss_features.h"
@@ -5042,6 +5043,7 @@ int omapdss_dsi_display_enable(struct omap_dss_device *dssdev)
 {
 	struct platform_device *dsidev = dsi_get_dsidev_from_dssdev(dssdev);
 	struct dsi_data *dsi = dsi_get_dsidrv_data(dsidev);
+	struct omap_display_platform_data *dss_plat_data;
 	int r = 0;
 
 	DSSDBG("dsi_display_enable\n");
@@ -5060,6 +5062,10 @@ int omapdss_dsi_display_enable(struct omap_dss_device *dssdev)
 	if (r)
 		goto err_get_dsi;
 
+	dss_plat_data = dsidev->dev.platform_data;
+	dss_plat_data->device_scale(&dssdev->dev,
+			omap_hwmod_name_get_dev("dss_dispc"),
+			dssdev->panel.timings.pixel_clock * 1000);
 
 	if(!dssdev->skip_init)
 #ifndef CONFIG_MACH_OMAP4_BOWSER_SUBTYPE_TATE
@@ -5106,6 +5112,8 @@ err_init_dsi:
 	dsi_display_uninit_dispc(dssdev);
 err_init_dispc:
 	dsi_enable_pll_clock(dsidev, 0);
+	dss_plat_data->device_scale(&dssdev->dev,
+			omap_hwmod_name_get_dev("dss_dispc"), 0);
 	dsi_runtime_put(dsidev);
 err_get_dsi:
 	omap_dss_stop_device(dssdev);
@@ -5180,6 +5188,7 @@ void omapdss_dsi_display_disable(struct omap_dss_device *dssdev,
 {
 	struct platform_device *dsidev = dsi_get_dsidev_from_dssdev(dssdev);
 	struct dsi_data *dsi = dsi_get_dsidrv_data(dsidev);
+	struct omap_display_platform_data *dss_plat_data;
 
 	DSSDBG("dsi_display_disable\n");
 
@@ -5194,6 +5203,9 @@ void omapdss_dsi_display_disable(struct omap_dss_device *dssdev,
 
 	dsi_display_uninit_dsi(dssdev, disconnect_lanes, enter_ulps);
 
+	dss_plat_data = dsidev->dev.platform_data;
+	dss_plat_data->device_scale(&dssdev->dev,
+			omap_hwmod_name_get_dev("dss_dispc"), 0);
 
 	dsi_runtime_put(dsidev);
 	dsi_enable_pll_clock(dsidev, 0);
