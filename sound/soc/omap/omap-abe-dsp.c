4a65f93973084a96e7c34ee9f2291109a4c1abd8
Hashcode <hashcode0f@gmail.com>
2012-12-18 11:18:20 -0800

    Revert "ASoc: ABE VxREC: use VxREC audio back-end"
    This reverts commit 891f51f95101bf9949b9582114acd7d16b323f57.

--- sound/soc/omap/omap-abe-dsp.c
+++ sound/soc/omap/omap-abe-dsp.c
@@ -1193,8 +1193,6 @@ static const struct snd_soc_dapm_widget abe_dapm_widgets[] = {
 			W_AIF_DMIC1, ABE_OPP_50, 0),
 	SND_SOC_DAPM_AIF_IN("DMIC2", "DMIC2 Capture", 0,
 			W_AIF_DMIC2, ABE_OPP_50, 0),
-	SND_SOC_DAPM_AIF_IN("VXREC", "VXREC Capture", 0,
-			W_AIF_VXREC, ABE_OPP_50, 0),
 
 	/* ROUTE_UL Capture Muxes */
 	SND_SOC_DAPM_MUX("MUX_UL00",
@@ -1524,10 +1522,10 @@ static const struct snd_soc_dapm_route intercon[] = {
 	{"PDM_DL2", NULL, "DL2 Mixer"},
 
 	/* VxREC Mixer */
-	{"Capture Mixer", "Tones", "VXREC"},
-	{"Capture Mixer", "Voice Playback", "VXREC"},
-	{"Capture Mixer", "Voice Capture", "VXREC"},
-	{"Capture Mixer", "Media Playback", "VXREC"},
+	{"Capture Mixer", "Tones", "TONES_DL"},
+	{"Capture Mixer", "Voice Playback", "VX DL VMixer"},
+	{"Capture Mixer", "Voice Capture", "VX UL VMixer"},
+	{"Capture Mixer", "Media Playback", "MM_DL VMixer"},
 	{"MM_DL VMixer", NULL, "MM_DL"},
 	{"MM_DL VMixer", NULL, "MM_DL_LP"},
 
@@ -1564,7 +1562,6 @@ static const struct snd_soc_dapm_route intercon[] = {
 	{"DMIC0", NULL, "BE_IN"},
 	{"DMIC1", NULL, "BE_IN"},
 	{"DMIC2", NULL, "BE_IN"},
-	{"VXREC", NULL, "BE_IN"},
 };
 
 #ifdef CONFIG_DEBUG_FS
