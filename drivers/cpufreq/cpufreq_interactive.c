02b2212082d3c1e7a9874e8af4b13e636019ebd8
Hashcode <hashcode0f@gmail.com>
2014-01-03 23:55:28 -0800

    Revert "OMAP4: DPLL cascading: Tune cpufreq interactive governor"
    This reverts commit 5ef1a97b92e80e1612e2c94f5af4bf23967b9b59.

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -113,9 +113,6 @@ static unsigned long min_sample_time;
  */
 #define DEFAULT_TIMER_RATE 20 * USEC_PER_MSEC
 static unsigned long timer_rate;
-#ifdef CONFIG_OMAP4_DPLL_CASCADING
-static unsigned long default_timer_rate;
-#endif
 
 static unsigned int boost_timeout;
 
@@ -133,19 +130,6 @@ struct cpufreq_governor cpufreq_gov_interactive = {
 	.max_transition_latency = 10000000,
 	.owner = THIS_MODULE,
 };
-
-#ifdef CONFIG_OMAP4_DPLL_CASCADING
-void cpufreq_interactive_set_timer_rate(unsigned long val, unsigned int reset)
-{
-	if (!reset) {
-		default_timer_rate = timer_rate;
-		timer_rate = val;
-	} else {
-		if (timer_rate == val)
-			timer_rate = default_timer_rate;
-	}
-}
-#endif
 
 static void cpufreq_interactive_timer(unsigned long data)
 {
@@ -1037,9 +1021,6 @@ static int __init cpufreq_interactive_init(void)
 	go_hispeed_load = DEFAULT_GO_HISPEED_LOAD;
 	min_sample_time = DEFAULT_MIN_SAMPLE_TIME;
 	timer_rate = DEFAULT_TIMER_RATE;
-#ifdef CONFIG_OMAP4_DPLL_CASCADING
-	default_timer_rate = DEFAULT_TIMER_RATE;
-#endif
 
 	sampling_periods = DEFAULT_SAMPLING_PERIODS;
 	hi_perf_threshold = DEFAULT_HI_PERF_THRESHOLD;
