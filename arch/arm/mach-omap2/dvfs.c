b5701c2a6524ecdfa7ca5c7993ae4797663c2105
Hashcode <hashcode0f@gmail.com>
2014-01-03 23:56:03 -0800

    Revert "OMAP4: DPLL cascading: introduce DPLL_ABE low power bypass/cascade"
    This reverts commit 90da89f1b2cc00cb2fcc2d7d31fc55777ad2fa23.

--- arch/arm/mach-omap2/dvfs.c
+++ arch/arm/mach-omap2/dvfs.c
@@ -28,9 +28,7 @@
 #include "smartreflex.h"
 #include "powerdomain.h"
 #include "pm.h"
-#ifdef CONFIG_OMAP4_DPLL_CASCADING
-#include <mach/omap4-common.h>
-#endif
+
 /**
  * DOC: Introduction
  * =================
@@ -972,11 +970,6 @@ int omap_device_scale(struct device *req_dev, struct device *target_dev,
 		dev_dbg(target_dev, "%s: pm is not ready yet\n", __func__);
 		return -EBUSY;
 	}
-
-#ifdef CONFIG_OMAP4_DPLL_CASCADING
-	if (omap4_is_in_dpll_cascading())
-		return -EBUSY;
-#endif
 
 	/* Lock me to ensure cross domain scaling is secure */
 	mutex_lock(&omap_dvfs_lock);
