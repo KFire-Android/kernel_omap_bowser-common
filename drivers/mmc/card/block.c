af1b465afa43de74c6dd4649df3c2a1f4c561f28
Hashcode <hashcode0f@gmail.com>
2014-12-23 09:52:12 -0800

    mmc: core: [partial revert] new discard feature support at Samsung eMMC v4.41+.
    Partially reverts:
    https://github.com/CyanogenMod/android_kernel_amazon_bowser-common/commit/25a68c7e5ed3a95bc8654a0b4f51be4fa5674bd0
    
    Change-Id: If32a31bfacbb17d302630c2b415bdf063ebe31a5

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -1165,16 +1165,9 @@ static int mmc_blk_issue_rq(struct mmc_queue *mq, struct request *req)
 	}
 
 	if (req->cmd_flags & REQ_DISCARD) {
-/*
-** HASH:
-** Patching *possible* TRIM bug in Samsung MAG2GA Chips
-** -- also removing the secdiscard request to be safe
-*/
-#if 0
 		if (req->cmd_flags & REQ_SECURE)
 			ret = mmc_blk_issue_secdiscard_rq(mq, req);
 		else
-#endif
 			ret = mmc_blk_issue_discard_rq(mq, req);
 	} else if (req->cmd_flags & REQ_FLUSH) {
 		ret = mmc_blk_issue_flush(mq, req);
